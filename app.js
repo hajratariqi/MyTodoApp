901eb057f3232c95c1051df0b0102761c1c88b40
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-08 20:47:53 +0500

    Added functions and get ids

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+let addTodoBtn = document.getElementById('addTodo'); 
+let todoInp = document.getElementById('todo');
+let ul = document.getElementById('todoList');
+let delAll = document.getElementById('delAll');
+let todo = document.getElementById('todo');
+
 const addTodo = () =>{
-    
+    if(todoInp.value){
+        let li = document.createElement('li');
+        let inp = document.createElement('input');
+        inp.setAttribute('disabled', true);
+        inp.setAttribute('type', 'text');
+        inp.setAttribute('value', todoInp.value);
+        inp.setAttribute('id', 'todo');
+        ul.appendChild(li);
+        li.appendChild(inp);
+    }
 }
